27e78cd8f6ea37019f489a96f53e9f27f2bfbb71
vijay-3 <mpvijay15092001@gmail.com>
2025-01-03 01:20:55 +0530

    Move the internal script code to external script.js file and added the arithmetic addition code

---
+++ 01_print_value_in_console/script.js
@@ -0,0 +1,18 @@
+// Variable
+// Print the variable value to the console
+let amount = 1000;
+console.log("Your bill amount is Rs." + amount);
+
+const PI = 3.14; // Const variable initialize the value when declaration
+console.log("The PI value is ", PI);
+
+var weight = 44;
+weight = 33;
+console.log("The weight is", weight);
+
+// Addition
+let add_num1 = 44;
+let add_num2 = 23;
+let add_sum = add_num1 + add_num2;
+console.log("Addition :", add_sum);
+console.log("Addition :", add_sum + 4);
