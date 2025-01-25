d09cfd45065343672ac190afd5d83a88ff2805dc
vijay-3 <mpvijay15092001@gmail.com>
2025-01-25 15:12:33 +0530

    Added the string with result

--- 02_length_convertor/script.js
+++ 02_length_convertor/script.js
@@ -3,5 +3,5 @@ function convert() {
   const centi_val = Number(document.getElementById("user_input_box").value);
   const inch_val = centi_val / 2.54;
   const result = document.getElementById("result_container");
-  result.innerHTML = inch_val.toFixed(2);
+  result.innerHTML = inch_val.toFixed(2) + "inch";
 }
