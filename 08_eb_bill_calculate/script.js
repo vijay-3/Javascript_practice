4b7e2b0d34180300526a5568488fc6c05563cdff
vijay <whatsappentertainment444@gmail.com>
2025-10-01 21:25:57 +0530

    Format the code

--- 08_eb_bill_calculate/script.js
+++ 08_eb_bill_calculate/script.js
@@ -5,25 +5,25 @@ let per_unit_151_to_250 = 1.3;
 let per_unit_251_to_above = 1.5;
 
 // Create the element for display the unit cost details
-const UNIT_DETAIL_AREA = document.createElement("div");
+const UNIT_DETAILS_AREA = document.createElement("div");
 
-// Style the UNIT_DETAIL_AREA element
-UNIT_DETAIL_AREA.style.backgroundColor = "#ffff80";
-UNIT_DETAIL_AREA.style.width = "maxcontent";
-UNIT_DETAIL_AREA.style.height = "maxcontent";
-UNIT_DETAIL_AREA.style.padding = "10px";
-UNIT_DETAIL_AREA.style.position = "absolute";
-UNIT_DETAIL_AREA.style.right = "10px";
-UNIT_DETAIL_AREA.style.bottom = "10px";
-UNIT_DETAIL_AREA.style.fontWeight = "bold";
-UNIT_DETAIL_AREA.style.borderRadius = "8px";
-UNIT_DETAIL_AREA.style.fontSize = "20px";
-UNIT_DETAIL_AREA.setAttribute("className", "unit_cost_detail");
-UNIT_DETAIL_AREA.innerHTML = `<pre><h3 style = "justify-self: center; color: red;">Unit Cost Detail</h3> <p>0 to 50 Unit      : ${per_unit_0_to_50}</p> <p>51 to 150 Unit    : ${per_unit_51_to_150}</p> <p>151 to 250 Unit   : ${per_unit_151_to_250}</p> <p>251 to above Unit : ${per_unit_251_to_above}</p></pre>`;
+// Style the UNIT_DETAILS_AREA element
+UNIT_DETAILS_AREA.style.backgroundColor = "#ffff80";
+UNIT_DETAILS_AREA.style.width = "maxcontent";
+UNIT_DETAILS_AREA.style.height = "maxcontent";
+UNIT_DETAILS_AREA.style.padding = "10px";
+UNIT_DETAILS_AREA.style.position = "absolute";
+UNIT_DETAILS_AREA.style.right = "10px";
+UNIT_DETAILS_AREA.style.bottom = "10px";
+UNIT_DETAILS_AREA.style.fontWeight = "bold";
+UNIT_DETAILS_AREA.style.borderRadius = "8px";
+UNIT_DETAILS_AREA.style.fontSize = "20px";
+UNIT_DETAILS_AREA.setAttribute("className", "unit_cost_detail");
+UNIT_DETAILS_AREA.innerHTML = `<pre><h3 style = "justify-self: center; color: red;">Unit Cost Detail</h3> <p>0 to 50 Unit      : ${per_unit_0_to_50}</p> <p>51 to 150 Unit    : ${per_unit_51_to_150}</p> <p>151 to 250 Unit   : ${per_unit_151_to_250}</p> <p>251 to above Unit : ${per_unit_251_to_above}</p></pre>`;
 
 // Get the whole page element and add the element
 const FULL_PAGE = document.getElementsByClassName("main_container")[0];
-FULL_PAGE.append(UNIT_DETAIL_AREA);
+FULL_PAGE.append(UNIT_DETAILS_AREA);
 
 // Submit button works without reload the page
 const SUBMIT_BUTTON = document.getElementById("user_input_form");
@@ -70,7 +70,7 @@ SUBMIT_BUTTON.addEventListener("submit", function (event) {
     unit_250_above = user_unit - 250;
     unit_250_above = unit_250_above * 1.5;
   }
-  
+
   // Here the output
   eb_cost = unit_50 + unit_150 + unit_250 + unit_250_above;
   OUTPUT.innerHTML = eb_cost.toFixed(2);
